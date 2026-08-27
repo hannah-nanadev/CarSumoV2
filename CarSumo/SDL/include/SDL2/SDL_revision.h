4ce2691c1cd35e1c0f49bd7cc54159c5f82705fa
hannah-nanadev <hannahkellett2@gmail.com>
2026-08-27 18:37:24 +0100

    🚧➕ beginning attempt at refactoring + attempt to add sdl2 on my end. failed. figuring this out later

---
+++ CarSumo/SDL/include/SDL2/SDL_revision.h
@@ -0,0 +1,6 @@
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "release-2.32.70-0-ga53b6ad (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "release-2.32.70-0-ga53b6ad"
+#endif
+#define SDL_REVISION_NUMBER 0
